e5b880978d27b4c99defc0424723bd26645e9460
Phillipp Engelke <zottelchin@gmail.com>
2019-01-22 23:35:33 +0100

    Basic parsing of events (name, location, start and finish)

--- parser.go
+++ parser.go
@@ -4,15 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println("hello")
-	Parse(3, "test", "test2")
+	Parse(3, "https://calendar.google.com/calendar/ical/rrlj3mbt8ohkiq9l10j597bavg@group.calendar.google.com/private-1e93e3ef97ce9779732182fe54c7b173/basic.ics", "test2")
 }
 
 func Parse(num int, urls ...string) {
-	//var a []Event
+	var a []Event
 	for _, url := range urls {
 		resp, err := http.Get(url)
 		if err != nil || resp.StatusCode != 200 {
@@ -21,7 +23,98 @@ func Parse(num int, urls ...string) {
 		defer resp.Body.Close()
 		scanner := bufio.NewScanner(resp.Body)
 		for scanner.Scan() {
-			fmt.Println(scanner.Text)
+			if strings.Contains(scanner.Text(), "BEGIN:VEVENT") {
+				event, _, _ := parseEvent(scanner)
+				a = append(a, event)
+
+			}
 		}
 	}
 }
+
+func parseEvent(scanner *bufio.Scanner) (Event, string, []string) {
+	var a Event
+	var rrule string
+	var exclude []string
+	for !strings.Contains(scanner.Text(), "END:VEVENT") {
+		line := scanner.Text()
+		switch {
+		case strings.HasPrefix(line, "DTSTART"): //StartZeit
+			if strings.Contains(line, "TZID") {
+				location := strings.SplitAfterN(strings.TrimPrefix(line, "DTSTART;TZID="), ":", 2)[0]
+				loc, _ := time.LoadLocation(location)
+				a.Start, _ = time.ParseInLocation(timeLayout, strings.SplitAfterN(line, ":", 2)[1], loc)
+			} else {
+				a.Start, _ = time.Parse(timeLayout, strings.SplitAfterN(line, ":", 2)[1])
+			}
+		case strings.HasPrefix(line, "DTEND"): //EndZeit
+			if strings.Contains(line, "TZID") {
+				location := strings.SplitAfterN(strings.TrimPrefix(line, "DTEND;TZID="), ":", 2)[0]
+				loc, _ := time.LoadLocation(location)
+				a.End, _ = time.ParseInLocation(timeLayout, strings.SplitAfterN(line, ":", 2)[1], loc)
+			} else {
+				a.End, _ = time.Parse(timeLayout, strings.SplitAfterN(line, ":", 2)[1])
+			}
+		case strings.HasPrefix(line, "LOCATION"): //Ort
+			a.Location = strings.SplitAfterN(line, ":", 2)[1]
+		case strings.HasPrefix(line, "DESCRIPTION"): //Beschreibung
+			a.Description = strings.SplitAfterN(line, ":", 2)[1]
+		case strings.HasPrefix(line, "SUMMARY"): //Titel
+			a.Name = strings.SplitAfterN(line, ":", 2)[1]
+		case strings.HasPrefix(line, "RRULE"): //Wiederholungsregel
+			rrule = strings.SplitAfterN(line, ":", 2)[1]
+		case strings.HasPrefix(line, "EXDATE"): //Ausnahme zu Wiederholungsregel
+			exclude = append(exclude, line)
+		}
+		scanner.Scan()
+	}
+	return a, rrule, exclude
+}
+
+/*
+func parseRRule(e Event, rrule string, ex []string) []Event {
+	var all []Event
+	not := parseExclude(ex)
+	var params map[string]string
+	params = make(map[string]string)
+
+	ss := strings.Split(rrule, ";")
+	for _, pair := range ss {
+		z := strings.Split(pair, "=")
+		params[z[0]] = z[1]
+	}
+
+	switch params["FREQ"] {
+	case "YEARLY": //Jährlich
+		if params["BYDAY"] == "" { //Datum und Monat. 12 Januar
+
+		}
+		if params["BYDAY"] != "" { //Wochentag und Anzahl der Vorkommnisse: 1. Sonntag  oder so
+			params["BYDAY"] //Tag der wiederholung
+			params["BYSETPOS"] //vorkommnis des Tags im Monat
+
+		}
+	case "MONTHLY": //Monatlich
+	case "WEEKLY": //Wöchentlich
+	case "DAILY": //Täglich
+	case "HOURLY": //Stündlich
+
+	}
+}
+
+func parseExclude(ex []string) []time.Time {
+	var ret []time.Time
+	for _, line := range ex {
+		var exdate time.Time
+		if strings.Contains(line, "TZID") {
+			location := strings.SplitAfterN(strings.TrimPrefix(line, "EXDATE;TZID="), ":", 2)[0]
+			loc, _ := time.LoadLocation(location)
+			exdate, _ = time.ParseInLocation(timeLayout, strings.SplitAfterN(line, ":", 2)[1], loc)
+		} else {
+			exdate, _ = time.Parse(timeLayout, strings.SplitAfterN(line, ":", 2)[1])
+		}
+		ret = append(ret, exdate)
+	}
+	return ret
+}
+*/
--- types.go
+++ types.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 type Event struct {
-	Name     string
-	Start    time.Time
-	End      time.Time
-	Location string
+	Name        string
+	Start       time.Time
+	End         time.Time
+	Location    string
+	Description string
+}
+
+const timeLayout string = "20060102T150405"
+
+func PrintEvent(e Event) {
+	fmt.Printf("%s findet vom %s bis zum %s an diesem Ort %s statt. Beschreibung: %s\n", e.Name, e.Start.String(), e.End.String(), e.Location, e.Description)
 }
